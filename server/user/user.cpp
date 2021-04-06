67edfe8b7d7c6b09050956c19b7f0054289594f0
ibuprofenlz <921555285@qq.com>
2021-04-06 19:14:53 +0800

    ...

--- server/user/user.cpp
+++ server/user/user.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 namespace user_s {
 user_s::user::user(){
+
     std::cout<<"一个纯路人用户出现";
 }
 
@@ -12,4 +13,22 @@ int user_s::user::LogIn(){
 int user_s::user::SendMessage(){
     return 0;
 }
+void user_s::user::SetBaseInformation() {
+    std::cout<<"请输入您的姓名:";
+    std::cin>>this->Name;
+    std::cout<<"请输入您的性别:";
+    std::cin>>this->Sex;
+    std::cout<<"请输入您的年龄:";
+    std::cin>> this->Age;
+    std::cout<<"请输入您的个人简介:";
+    std::cin>>this->Intro;
+
+}
+void user_s::user::PrintInfo() {
+    std::cout<<this->Name<<std::endl;
+    std::cout<<this->Sex<<std::endl;
+    std::cout<<this->Age<<std::endl;
+    std::cout<<this->Intro<<std::endl;
+}
+
 }
--- server/user/user.h
+++ server/user/user.h
@@ -6,11 +6,23 @@
 #define INN_USER_H
 namespace user_s {
 class user{
-    public:
+public:
     user();
+    void SetBaseInformation();
+    void SetAccountInfo();
+    void PrintInfo();
     int LogIn();
     int SendMessage();
     int Exit();
+
+private:
+    char Name[20];
+    char Sex;
+    int Age;
+    int Account;
+    int Password;
+    char Intro[200];
+
 };
 }
 #endif //INN_USER_H
