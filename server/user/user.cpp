464d882a25f7c62952ebf014cf694aa2d9671594
ibuprofenlz <921555285@qq.com>
2021-04-06 16:38:34 +0800

    啥都没干

---
+++ server/user/user.cpp
@@ -0,0 +1,15 @@
+#include"user.h"
+#include<iostream>
+namespace user_s {
+user_s::user::user(){
+    std::cout<<"一个纯路人用户出现";
+}
+
+int user_s::user::LogIn(){
+    return 0;
+}
+
+int user_s::user::SendMessage(){
+    return 0;
+}
+}
---
+++ server/user/user.h
@@ -0,0 +1,17 @@
+//
+// Created by ibuprofenlz on 2021/4/6.
+//
+
+#ifndef INN_USER_H
+#define INN_USER_H
+namespace user_s {
+class user{
+    public:
+    user();
+    int LogIn();
+    int SendMessage();
+    int Exit();
+};
+}
+#endif //INN_USER_H
+
