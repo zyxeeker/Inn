c22ed96f63357962a80bab5843c25c654900225d
zyxeeker <190474450@qq.com>
2021-04-08 16:39:52 +0800

    ✔初始化线程池的构建

---
+++ server/thread/locker.h
@@ -0,0 +1,8 @@
+//
+// Created by ZYXeeker on 2021/4/8.
+//
+
+#ifndef INN_LOCKER_H
+#define INN_LOCKER_H
+
+#endif //INN_LOCKER_H
---
+++ server/thread/thread_pool.h
@@ -0,0 +1,13 @@
+//
+// Created by ZYXeeker on 2021/4/8.
+//
+
+#ifndef INN_THREAD_POOL_H
+#define INN_THREAD_POOL_H
+
+template<typename T>
+class thread_pool {
+
+};
+
+#endif //INN_THREAD_POOL_H
