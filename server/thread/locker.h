62fa930306140076a77f3bf757f96486f2803aa2
zyxeeker <190474450@qq.com>
2021-04-09 16:10:13 +0800

    🧱线程与锁

--- server/thread/locker.h
+++ server/thread/locker.h
@@ -5,4 +5,64 @@
 #ifndef INN_LOCKER_H
 #define INN_LOCKER_H
 
+#include <pthread.h>
+#include <semaphore.h>
+#include <exception>
+
+// 信号量
+class sem {
+public:
+    sem() {
+        if (sem_init(&m_sem, 0, 0) != 0) throw std::exception();
+    }
+
+    sem(int num) {
+        if (sem_init(&m_sem, 0, num)) throw std::exception();
+    }
+
+    ~sem() {
+        sem_destroy(&m_sem);
+    }
+
+    // P信号
+    bool P() {
+        return sem_wait(&m_sem) == 0;
+    }
+
+    // V信号
+    bool V() {
+        return sem_post(&m_sem) == 0;
+    }
+
+private:
+    sem_t m_sem;
+};
+
+// 互斥锁 mutex
+class locker {
+public:
+    locker() {
+        if (pthread_mutex_init(&m_mutex, nullptr) != 0) throw std::exception();
+    }
+
+    ~locker() {
+        pthread_mutex_destroy(&m_mutex);
+    }
+
+    // 加锁
+    bool lock() {
+        return pthread_mutex_lock(&m_mutex) == 0;
+    }
+
+    // 解锁
+    bool unlock() {
+        return pthread_mutex_unlock(&m_mutex) == 0;
+    }
+
+
+private:
+    pthread_mutex_t m_mutex;
+
+};
+
 #endif //INN_LOCKER_H
--- server/thread/thread.h
+++ server/thread/thread.h
@@ -5,9 +5,96 @@
 #ifndef INN_THREAD_H
 #define INN_THREAD_H
 
-template<typename T>
-class thread {
+#include <pthread.h>
+#include "locker.h"
+#include <list>
 
+template<typename T>
+class thread_pool {
+public:
+    thread_pool(int thread_num, int request_num) : m_THREAD_NUM(thread_num), m_MAX_REQUEST_NUM(request_num) {};
+
+    bool append_work(T *req);
+
+    ~thread_pool() { delete[] m_threads; };
+
+private:
+    static void *worker(void *args);
+
+    void run();
+
+private:
+    // 线程数
+    int m_THREAD_NUM;
+    // 最大请求数
+    int m_MAX_REQUEST_NUM;
+    // 线程队列
+    std::list<T *> m_work_queue;
+    // 线程数组
+    pthread_t *m_threads;
+    // 锁
+    locker m_locker;
+    // 信号量
+    sem m_sem;
+    bool m_run_statue{true};
 };
+
+template<typename T>
+thread_pool<T>::thread_pool(int thread_num, int request_num) {
+    m_threads = new pthread_t[m_THREAD_NUM];
+
+    for (int i = 0; i < m_THREAD_NUM; ++i) {
+        if (pthread_create(m_threads + i, nullptr, worker, this) != 0) {
+            delete[] m_threads;
+        }
+
+        pthread_detach(m_threads[i]);
+    }
+}
+
+template<typename T>
+thread_pool<T>::~thread_pool<T>() {
+    delete[] m_threads;
+}
+
+template<typename T>
+bool thread_pool<T>::append_work(T *req) {
+    m_locker.lock();
+
+    m_work_queue.push_back(req);
+
+    m_locker.unlock();
+
+    m_sem.V();
+    return true;
+}
+
+template<typename T>
+bool thread_pool<T>::worker(void *args) {
+    thread_pool *pool = (thread_pool *) args;
+    pool->run();
+    return pool;
+}
+
+template<typename T>
+void thread_pool<T>::run() {
+    while (m_run_statue) {
+        m_sem.P();
+
+        m_locker.lock();
+
+        if (m_work_queue.empty()) {
+            m_locker.unlock();
+            continue;
+        }
+
+        T *req = m_work_queue.front();
+        m_work_queue.pop_front();
+
+        m_locker.unlock();
+
+        req->do_request();
+    }
+}
 
 #endif //INN_THREAD_H
